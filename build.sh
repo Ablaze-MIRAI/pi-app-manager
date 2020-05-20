db38e9e887c98658432b3954332f32ab02e8aaf1
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-05-20 17:57:30 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-set -e
-if [[ $UID == 0 ]]; then
-    echo "You must not run this as root." 1>&2
-    exit 1
-fi
-
-if !(type docker >/dev/null); then
-    echo "Docker is not installing on this machine." 1>&2
-    exit 1
-fi
-WORKDIR=$(cd $(dirname $0); pwd)
-NAME=serene-startdash
-cd ..
-sudo docker build -t build_serenestartdash ${WORKDIR}
-sudo docker run -e NAME=$NAME -e UGID="${UID}:$(id -u)" -v ${WORKDIR}:/debuild/build/${NAME}-source:ro -v ${WORKDIR}/out:/deb -it build_serenestartdash
+sudo dpkg -b ./
