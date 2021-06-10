b7deb3dab97075774550e6fdf4c7a3a92be94e38
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2021-06-10 22:57:39 +0900

    Update pi-app-manager-portable.bash

--- run/pi-app-manager-portable.bash
+++ run/pi-app-manager-portable.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt install git -y
-tag="$(curl -s https://raw.githubusercontent.com/NSK-1010/pi-app-manager/master/run/version)"
-git clone https://github.com/nsk-1010/pi-app-manager.git -b ${tag}
+piappmanager_version="$(curl -s https://raw.githubusercontent.com/NSK-1010/pi-app-manager/master/run/version)"
+git clone https://github.com/nsk-1010/pi-app-manager.git -b ${piappmanager_version}
 chmod 755 -R pi-app-manager
 cd pi-app-manager
 ./depends.sh
