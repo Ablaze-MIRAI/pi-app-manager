3c0fcc770882395e5463a95199bdb879989a5e8d
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2021-06-10 20:51:45 +0900

    Update pi-app-manager.bash

--- run/pi-app-manager.bash
+++ run/pi-app-manager.bash
@@ -4,7 +4,7 @@ git clone https://github.com/nsk-1010/pi-app-manager.git -b 1.10-2
 chmod 755 -R pi-app-manager
 cd pi-app-manager
 scriptdir=$(pwd)
-./depends.bash
+./depends.sh
 ./pi-app-manager
 echo "今後は 'bash ${scriptdir}/pi-app-manager' というコマンドを実行するとPi-App-Managerが起動します。"
 cd ..
