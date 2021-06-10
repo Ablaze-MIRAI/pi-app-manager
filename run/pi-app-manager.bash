4c293c2856cacd4d7a474de86ca91183a22f7d01
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2021-06-10 22:20:11 +0900

    Update pi-app-manager.bash

--- run/pi-app-manager.bash
+++ run/pi-app-manager.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+source ./version.bash
 apt install git -y
-git clone https://github.com/nsk-1010/pi-app-manager.git -b 1.10-3
+git clone https://github.com/nsk-1010/pi-app-manager.git -b ${tag}
 chmod 755 -R pi-app-manager
 cd pi-app-manager
 scriptdir=$(pwd)
