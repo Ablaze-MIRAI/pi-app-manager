8e293d8f6f548bd974f06d41b75a135dab2bb115
猫宮ぴあの / Piano Nekomiya <61345869+NSK-1010@users.noreply.github.com>
2022-06-27 02:43:24 +0900

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 echo "依存パッケージをインストールします。"
-apt install bash jq zenity git -y --no-install-recommends
+apt install bash jq zenity git curl -y --no-install-recommends
 
+mkdir pacapt-installer
 curl -o ./pacapt-installer/installer.bash https://raw.githubusercontent.com/Hayao0819/pacapt-installer/master/pacapt-installer.bash
 chmod +x ./pacapt-installer/installer.bash
-bash ./pacapt-installer/installer.bash
+bash ./pacapt-installer/installer.bash 2
+rm -r pacapt-installer
 
 echo "インストールが完了しました。"
